4306c4c51124838fbb5ede4e032cdd14f5da07dc
scorp <scorp@FabianCS>
2024-12-31 16:56:36 -0600

    primeras plantillas

---
+++ src/main/java/com/mycompany/sanantonioplasticos/SanAntonioPlasticos.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sanantonioplasticos;
+
+public class SanAntonioPlasticos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
